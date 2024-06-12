7dcfa7ccff5e893210cd506cb618b4f27a5cba94
davi-canuto <davicanutogregorio@gmail.com>
2024-06-12 15:39:00 -0300

    chore: initialize gem

---
+++ lib/ruby/telegram/bot.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "bot/version"
+
+module Ruby
+  module Telegram
+    module Bot
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
